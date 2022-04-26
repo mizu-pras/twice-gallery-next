7ba3b285a3e93da2290b40fa3dcfe48ff8ec60c4
dimas <1-dimas@hris-system.com>
2022-04-26 16:49:46 +0700

    update nih

--- pages/[menu]/[slug].js
+++ pages/[menu]/[slug].js
@@ -1,41 +1,46 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+
 import styles from '../../styles/Gallery.module.css'
 
-const Slug = ({ data, page, totalPage }) => {
-    const [dataShow, setDataShow] = useState([])
+const Slug = () => {
+    const router = useRouter()
+    const { menu, slug } = router.query
+
+    const [data, setData] = useState([])
+    const [page, setPage] = useState(1)
+    const [totalPage, setTotalPage] = useState(0)
 
     useEffect(() => {
-        setDataShow(prev => [...prev, ...data])
-    }, [data])
+        if (menu && slug) {
+            const fetchData = () => {
+                fetch(`/api/${menu}/${slug}`)
+                    .then((res) => {
+                        if (res.ok) {
+                            return res.json();
+                        }
+                        throw new Error('Something went wrong')
+                    })
+                    .then((data) => {
+                        setData(data.data)
+                        setPage(data.page)
+                        setTotalPage(data.totalPage)
+                    })
+                    .catch(err => {
+                        console.error(err)
+                    })
+            }
+    
+            fetchData()
+        }
+    }, [menu, slug, page])
 
     return (
         <div>
-            
+            {menu}
+            {slug}
         </div>
     )
-}
-
-export async function getServerSideProps(context) {
-    const { menu, slug } = context.query
-
-    const url = process.env.USE_HOSTNAME
-
-    const res = await fetch(`${url}/api/${menu}/${slug}`)
-    const data = await res.json()
-  
-    if (!data.data) {
-        return {
-            notFound: true,
-        }
-    }
-  
-    return {
-        props: {
-            data: data.data,
-            page: data.page,
-            totalPage: data.totalPage
-        }
-    }
 }
 
 export default Slug
