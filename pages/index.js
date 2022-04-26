6f6ecd56182fdcc6c1d6a73ae41236476e433c20
dimas <1-dimas@hris-system.com>
2022-04-26 13:34:56 +0700

    fix fetch data menu

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,18 @@ import styles from '../styles/Home.module.css'
 
 import HomeGallery from '../components/HomeGallery'
 
-export default function Home({ menus }) {
-	const [activeMenu, setACtiveMenu] = useState(menus[0])
+export default function Home() {
+	const [menus, setMenus] = useState([])
+	const [activeMenu, setACtiveMenu] = useState(null)
 	const [data, setData] = useState(null)
-  	const [isLoading, setLoading] = useState(false)
+	const [isInit, setIsInit] = useState(true)
+	const [isLoading, setLoading] = useState(false)
 	const [columnWidth, setColumnWidth] = useState()
 	const [containerHeight, setContainerHeight] = useState(0)
 	const { view, width, height } = useWindowDimensions()
 
-	const fetchDataSlug = async () => {
-		setLoading(true)
-		setData([])
-
-		fetch(`api/${activeMenu.path}`)
+	useEffect(() => {
+		fetch('api/menus')
 			.then((res) => {
 				if (res.ok) {
 					return res.json();
@@ -25,43 +24,67 @@ export default function Home({ menus }) {
 				throw new Error('Something went wrong')
 			})
 			.then((data) => {
-				setData(data)
-				setLoading(false)
+				setMenus(data)
+				setACtiveMenu(data[0])
+				setIsInit(false)
 			})
 			.catch(err => {
-				setLoading(false)
+				console.error(err)
+				setIsInit(false)
 			})
-	}
+	}, [])
 
 	useEffect(() => {
-		fetchDataSlug()
+		if (activeMenu) {
+			const fetchDataSlug = async () => {
+				setLoading(true)
+				setData([])
+		
+				fetch(`api/${activeMenu.path}`)
+					.then((res) => {
+						if (res.ok) {
+							return res.json();
+						}
+						throw new Error('Something went wrong')
+					})
+					.then((data) => {
+						setData(data)
+						setLoading(false)
+					})
+					.catch(err => {
+						setLoading(false)
+					})
+			}
+	
+			fetchDataSlug()
+		}
 	}, [activeMenu])
-
-	const handlerSetColumnWidthAndHeight = () => {
-		console.log('width', width, 'height', height)
-
-		let perColumn
-		let totalRow
-		if (view === 'lg') {
-			perColumn = (1200 - 56) / 3
-			totalRow = Math.ceil(data.length / 3)
-		}
-		else if (view === 'md') {
-			perColumn = (width - 56) / 3
-			totalRow = Math.ceil(data.length / 3)
-		}
-		else {
-			perColumn = (width - 14) / 2
-			totalRow = Math.ceil(data.length / 2)
-		}
-
-		setColumnWidth(perColumn)
-		setContainerHeight(perColumn * totalRow)
-		 
-	}
 
 	useEffect(() => {
 		if (data && data.length > 0) {
+			const handlerSetColumnWidthAndHeight = () => {
+				console.log('width', width, 'height', height)
+		
+				let perColumn
+				let totalRow
+				if (view === 'lg') {
+					perColumn = (1200 - 56) / 3
+					totalRow = Math.ceil(data.length / 3)
+				}
+				else if (view === 'md') {
+					perColumn = (width - 56) / 3
+					totalRow = Math.ceil(data.length / 3)
+				}
+				else {
+					perColumn = (width - 14) / 2
+					totalRow = Math.ceil(data.length / 2)
+				}
+		
+				setColumnWidth(perColumn)
+				setContainerHeight(perColumn * totalRow)
+				 
+			}
+			
 			handlerSetColumnWidthAndHeight()
 		}
 	}, [width, height, view, data])
@@ -118,42 +141,41 @@ export default function Home({ menus }) {
 					<h1>TWICE <span className='subtitle'>Photo Collection</span></h1>
 				</div>	
 
-				<div className='layout'>
-					<div className={styles.homeMenuWrapper}>
-						{
-							menus.map(menu => (
-								<button 
-									key={menu.path}
-									type='button' 
-									className={`${styles.buttonMenu}${activeMenu.path === menu.path ? ' ' + styles.buttonMenuActive : ''}`}
-									onClick={() => setACtiveMenu(menu)}
-								>
-									{menu.name}
-								</button>
-							))
-						}
-					</div>
-
-					{ isLoading && (
-						<div className={styles.homeLoadingWrapper}>
-							<span>Loading...</span>
+				{
+					isInit ? (
+						<div className={`layout ${styles.homeInitWrapper}`}>
+							<p>Loading...</p>
 						</div>
-					 ) }
+					) : (
+						<div className='layout'>
+							<div className={styles.homeMenuWrapper}>
+								{
+									menus.map(menu => (
+										<button 
+											key={menu.path}
+											type='button' 
+											className={`${styles.buttonMenu}${activeMenu.path === menu.path ? ' ' + styles.buttonMenuActive : ''}`}
+											onClick={() => setACtiveMenu(menu)}
+										>
+											{menu.name}
+										</button>
+									))
+								}
+							</div>
 
-					{renderHomeGallery()}
-				</div>
+							{ isLoading && (
+								<div className={styles.homeLoadingWrapper}>
+									<span>Loading...</span>
+								</div>
+							) }
+
+							{renderHomeGallery()}
+						</div>
+					)
+				}
+
+				
 			</main>
 		</div>
 	)
-}
-
-export async function getStaticProps(context) {
-	const res = await fetch('http://localhost:3000/api/menus')
-	const menus = await res.json()
-
-	return {
-	  props: {
-		  menus
-	  }
-	}
 }
