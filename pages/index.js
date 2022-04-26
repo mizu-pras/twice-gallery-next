3647dc0028de9bdda0aed560a92458a16f821917
dimas <1-dimas@hris-system.com>
2022-04-26 15:28:32 +0700

    update animation

--- pages/index.js
+++ pages/index.js
@@ -114,11 +114,10 @@ export default function Home() {
 
 		return (
 			<motion.div 
-				initial={{ opacity: 0, x: 0, y: 20 }}
+				initial={{ opacity: 0, x: 0, y: 10 }}
 				animate={{ opacity: 1, x: 0, y: 0 }}
-				exit={{ opacity: 0, x: 0, y: 20 }}
 				transition={{ 
-					duration: 0.4,  
+					duration: 0.2,  
 					type: 'easeInOut'
 				}}
 			>
