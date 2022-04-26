f1206c950b2f27205032f740cb36f3ccaef7ff01
dimas <1-dimas@hris-system.com>
2022-04-26 16:36:02 +0700

    set env

--- pages/[menu]/[slug].js
+++ pages/[menu]/[slug].js
@@ -1,15 +1,41 @@
-import { useRouter } from "next/router"
+import { useEffect, useState } from 'react'
+import styles from '../../styles/Gallery.module.css'
 
-const Slug = () => {
-    const router = useRouter()
-    const { menu, slug } = router.query
+const Slug = ({ data, page, totalPage }) => {
+    const [dataShow, setDataShow] = useState([])
+
+    useEffect(() => {
+        setDataShow(prev => [...prev, ...data])
+    }, [data])
 
     return (
         <div>
-            {menu}
-            {slug}
+            
         </div>
     )
+}
+
+export async function getServerSideProps(context) {
+    const { menu, slug } = context.query
+
+    const url = process.env.USE_HOSTNAME
+
+    const res = await fetch(`${url}/api/${menu}/${slug}`)
+    const data = await res.json()
+  
+    if (!data.data) {
+        return {
+            notFound: true,
+        }
+    }
+  
+    return {
+        props: {
+            data: data.data,
+            page: data.page,
+            totalPage: data.totalPage
+        }
+    }
 }
 
 export default Slug
