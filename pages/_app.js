beef793da9e450647579eddff6d376c59d143db7
dimas <1-dimas@hris-system.com>
2022-05-13 14:30:21 +0700

    add progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
+import dynamic from 'next/dynamic'
+const ProgressBar = dynamic(() => import('../components/ProgessBar'), { ssr: false })
+
 import { AactiveMenuWrapper } from '../context/activeMenu'
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <AactiveMenuWrapper>
-      <Component {...pageProps} />
-    </AactiveMenuWrapper>
-  )
+	return (
+		<>
+			<AactiveMenuWrapper>
+				<Component {...pageProps} />
+			</AactiveMenuWrapper>
+
+			<ProgressBar />
+		</>
+	)
 }
 
 export default MyApp
